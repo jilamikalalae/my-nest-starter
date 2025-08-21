1a5eb2c99dfd6ab035b0de37c728921c81355743
Guitar <jilamikalalae@gmail.com>
2025-08-21 17:21:37 +0700

    create first products without mongo

---
+++ src/products/products.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class ProductsService {
+    private readonly products = [
+        { id: 1, name: 'Product 1', description: 'Description 1' },
+        { id: 2, name: 'Product 2', description: 'Description 2' },
+        
+    ];
+
+    findAll() {
+        return this.products;
+    }
+
+    create(product) {
+        this.products.push(product);
+        return this.products;
+    }
+}
